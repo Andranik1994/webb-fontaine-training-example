8df5eefbfa6aded34d06bfeb956cf659a20eeaa0
Andranik <andranik.alajajyan@webbfontaine.com>
2020-09-26 12:18:34 +0400

    String Homework Begin.

---
+++ src/stringhomework/Task6.java
@@ -0,0 +1,10 @@
+package stringhomework;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        System.out.println("---Task_6---");
+
+        System.out.println();
+    }
+}
